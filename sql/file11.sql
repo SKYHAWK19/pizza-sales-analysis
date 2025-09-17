eb7851766ac6b8fb5f86804f8bae82e1463c3c60
ARIN CHAKRABORTY <156217362+SKYHAWK19@users.noreply.github.com>
2025-09-17 13:41:16 +0530

    upload sql files

---
+++ sql/file11.sql
@@ -0,0 +1,20 @@
+-- Calculate the percentage contribution of each pizza type to total revenue.
+
+SELECT 
+    pizza_types.category,
+    (((SUM(order_details.quantity * pizzas.price)) / (SELECT 
+            ROUND(SUM(order_details.quantity * pizzas.price),
+                        2) AS total_revenue
+        FROM
+            order_details
+                JOIN
+            pizzas ON pizzas.pizza_id = order_details.pizza_id)) * 100) AS revenue
+FROM
+    pizzas
+        JOIN
+    pizza_types ON pizzas.pizza_type_id = pizza_types.pizza_type_id
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizza_types.category
+ORDER BY revenue DESC
+;
---
+++ sql/file8.sql
@@ -0,0 +1,3 @@
+-- Join relevant tables to find the category-wise distribution of pizzas
+
+select count(name), category from pizza_types group by category
